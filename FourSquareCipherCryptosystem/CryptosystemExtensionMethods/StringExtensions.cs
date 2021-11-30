e2588ed2bb422369eebea7734596f61009cff33f
Jovana Spasić <87784195+J-Spasic@users.noreply.github.com>
2021-12-01 00:25:21 +0100

    [FourSquareCipherCryptosystem] Add extension methods project

---
+++ FourSquareCipherCryptosystem/CryptosystemExtensionMethods/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace CryptosystemExtensionMethods
+{
+    public static class StringExtensions
+    {
+        #region Method(s)
+        /// <summary>
+        /// Gets only letters from given input string.
+        /// </summary>
+        /// <param name="inputString">String used to extract letters.</param>
+        /// <returns>Letters from the input string if there are any; otherwise an empty string.</returns>
+        public static string GetLettersOnly(this string inputString) =>
+            new(inputString.Where(character => char.IsLetter(character)).ToArray());
+        #endregion Method(s)
+    }
+}
