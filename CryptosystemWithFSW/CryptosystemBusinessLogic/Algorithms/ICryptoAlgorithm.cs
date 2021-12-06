34c36422823d8b20fac3caf89d3bfce0d8119d5b
Jovana Spasić <87784195+J-Spasic@users.noreply.github.com>
2021-12-06 17:05:21 +0100

    [CryptosystemWithFSW] Add Four-Square Cipher

---
+++ CryptosystemWithFSW/CryptosystemBusinessLogic/Algorithms/ICryptoAlgorithm.cs
@@ -0,0 +1,8 @@
+﻿namespace CryptosystemBusinessLogic.Algorithms
+{
+    public interface ICryptoAlgorithm
+    {
+        string Encrypt(string sourceFileName, string plainText);
+        string Decrypt(string sourceFileName, string cipherText);
+    }
+}
